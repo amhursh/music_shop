ea7f8480ae533963f71a7422085b2e12aee4b7d9
limsammy1@gmail.com <limsammy1@gmail.com>
2017-10-16 15:10:12 -0700

    Added and migrated audios

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016215842) do
+ActiveRecord::Schema.define(version: 20171016220943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "audios", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "path"
+    t.index ["name"], name: "index_audios_on_name"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
---
+++ spec/factories/audios.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :audio do
+    
+  end
+end
